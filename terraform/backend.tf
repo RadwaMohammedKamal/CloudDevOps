56fe78df22cf0ea29ea8360074751eb0e37e8158
devops <devops@servera.example.com>
2026-02-09 19:33:10 +0200

    update yaml  9-3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket = "cloud-devops-nti-s3"
     key    = "terraform/terraform.tfstate"
     region = "eu-north-1"
-    # dynamodb_table = "terraform-locks"
     encrypt = true
   }
 }
